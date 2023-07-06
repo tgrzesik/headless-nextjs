862b1d5b62e62400dcc1c2a2eb2eaccf84f2bb74
Diarmuid Mac Namara <diarmuid.macnamara@wpengine.com>
2023-07-06 14:23:04 +0100

    Add redirect and rewrites

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/rewrite',
+        destination: '/',
+      },
+      {
+        source: '/blog/:slug',
+        destination: 'https://example.com/blog/:slug', // Matched parameters can be used in the destination
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/redirect',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   // output: 'standalone',
 }
 
