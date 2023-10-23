97b427f544bf2130a9c6ae094991f041dc057860
Diarmuid Mac Namara <diarmuid.macnamara@wpengine.com>
2023-10-23 13:56:13 +0100

    Add index page

---
+++ js/basic/pages/sample-isr/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head'
+
+export default function Home({generated, draftMode}) {
+  return (
+    <div className="container">
+      <Head>
+        <title>ISR</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+            (ISR) Server Side Generated page
+        </h1>
+        <p>Revalidated every 28 seconds, last revalidate: {generated} (DraftMode: {draftMode})</p>
+      </main>
+    </div>
+  )
+}
+
+// This function gets called at build time on server-side.
+// It may be called again, on a serverless function, if
+// revalidation is enabled and a new request comes in
+export async function getStaticProps(context) {
+  let time = new Date();
+  const generated = time.toTimeString()
+  let draftMode = "false"
+
+  if (context.draftMode) {
+    draftMode = "true"
+  }
+
+  return {
+    props: {
+      generated,
+      draftMode
+    },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 28, // In seconds
+  }
+}
