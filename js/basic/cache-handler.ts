2f74c88b5799952e5810257d13b8eaa6b8b979c0
Diarmuid Mac Namara <diarmuid.macnamara@wpengine.com>
2023-11-20 14:28:08 +0000

    Use local cache

--- js/basic/cache-handler.ts
+++ js/basic/cache-handler.ts
@@ -1,25 +1,10 @@
-const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const axios = require('axios');
 
 module.exports = class CacheHandler {
   options = {}
-  s3Client
-  bucket = process.env.bucket
 
   constructor(options) {
     this.options = options
-
-    const accountKey = process.env.key
-    const accountSecret = process.env.secret
-    const endpoint = process.env.endpoint
-
-    this.s3Client = new S3Client({
-      region: "auto",
-      endpoint: endpoint,
-      credentials: {
-        accessKeyId: accountKey,
-        secretAccessKey: accountSecret,
-      },
-    });
   }
 
   async get(key) {
@@ -27,18 +12,13 @@ module.exports = class CacheHandler {
     key = this.getKey("cache/" + key)
     console.log(`GET: ${key}`)
 
-    const command = new GetObjectCommand({
-      "Bucket": this.bucket,
-      "Key": key
+    axios.get(`http://localhost:8083/kv/${key}`)
+    .then(res => {
+      return res.data;
+    })
+    .catch(err => {
+      console.log('Error: ', err.message);
     });
-    try {
-      const response = await this.s3Client.send(command);
-      const str = await response.Body.transformToString();
-      const json = JSON.parse(str)
-      return json
-    } catch(e) {
-      console.log("Get failed")
-    }
   }
 
   async set(key, data, ctx) {
@@ -50,17 +30,13 @@ module.exports = class CacheHandler {
     key = this.getKey("cache/" + key)
     console.log(`SET: ${key}`, ctx)
 
-    const command = new PutObjectCommand({
-      "Bucket": this.bucket,
-      "Key": key,
-      "Body": JSON.stringify(payload),
-      "ContentType": 'application/json'
+    axios.put(`http://localhost:8083/kv/${key}`, data)
+    .then(res => {
+      return res.data;
+    })
+    .catch(err => {
+      console.log('Error: ', err.message);
     });
-    try {
-      await this.s3Client.send(command);
-    } catch (e) {
-      console.log("Put failed:", e)
-    }
   }
 
   async revalidateTag(tag) {
