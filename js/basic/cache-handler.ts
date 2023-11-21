2421811d29b5b60e2df2c4c8bfc8240cfa2c29df
Diarmuid Mac Namara <diarmuid.macnamara@wpengine.com>
2023-11-21 09:18:30 +0000

    fix timing

--- js/basic/cache-handler.ts
+++ js/basic/cache-handler.ts
@@ -15,21 +15,21 @@ module.exports = class CacheHandler {
     const instance = axios.create()
 
     instance.interceptors.request.use((config) => {
-        config.headers['request-startTime'] = process.hrtime()
+        config.headers['request-startTime'] = process.hrtime.bigint().toString()
         return config
     })
 
     instance.interceptors.response.use((response) => {
-        const start = response.config.headers['request-startTime']
-        const end = process.hrtime(start)
-        const milliseconds = Math.round((end[0] * 1000) + (end[1] / 1000000))
+        const start = BigInt(response.config.headers['request-startTime'])
+        const end = process.hrtime.bigint()
+        const milliseconds = Number(end - start) / 1000000;
         response.headers['request-duration'] = milliseconds
         return response
     })
 
     instance.get(`http://localhost:8083/kv/${key}`)
     .then(res => {
-      console.log(res.headers['request-duration'])
+      console.log(`Cache Duration: ${res.headers['request-duration']}ms`)
       return res.data;
     })
     .catch(err => {
@@ -49,21 +49,21 @@ module.exports = class CacheHandler {
     const instance = axios.create()
 
     instance.interceptors.request.use((config) => {
-        config.headers['request-startTime'] = process.hrtime()
-        return config
-    })
+      config.headers['request-startTime'] = process.hrtime.bigint().toString()
+      return config
+  })
 
-    instance.interceptors.response.use((response) => {
-        const start = response.config.headers['request-startTime']
-        const end = process.hrtime(start)
-        const milliseconds = Math.round((end[0] * 1000) + (end[1] / 1000000))
-        response.headers['request-duration'] = milliseconds
-        return response
-    })
+  instance.interceptors.response.use((response) => {
+      const start = BigInt(response.config.headers['request-startTime'])
+      const end = process.hrtime.bigint()
+      const milliseconds = Number(end - start) / 1000000;
+      response.headers['request-duration'] = milliseconds
+      return response
+  })
 
     instance.put(`http://localhost:8083/kv/${key}`, payload)
     .then(res => {
-      console.log(res.headers['request-duration'])
+      console.log(`Cache Duration: ${res.headers['request-duration']}ms`)
     })
     .catch(err => {
       console.log('Error: ', err.message);
