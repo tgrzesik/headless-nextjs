de1dd5ecba92bddb974f3830f8d575020641ae4c
Diarmuid Mac Namara <diarmuid.macnamara@wpengine.com>
2023-11-21 08:53:44 +0000

    Add timing info

--- js/basic/cache-handler.ts
+++ js/basic/cache-handler.ts
@@ -12,8 +12,24 @@ module.exports = class CacheHandler {
     key = this.getKey("cache/" + key)
     console.log(`GET: ${key}`)
 
-    axios.get(`http://localhost:8083/kv/${key}`)
+    const instance = axios.create()
+
+    instance.interceptors.request.use((config) => {
+        config.headers['request-startTime'] = process.hrtime()
+        return config
+    })
+
+    instance.interceptors.response.use((response) => {
+        const start = response.config.headers['request-startTime']
+        const end = process.hrtime(start)
+        const milliseconds = Math.round((end[0] * 1000) + (end[1] / 1000000))
+        response.headers['request-duration'] = milliseconds
+        return response
+    })
+
+    instance.get(`http://localhost:8083/kv/${key}`)
     .then(res => {
+      console.log(res.headers['request-duration'])
       return res.data;
     })
     .catch(err => {
@@ -30,7 +46,25 @@ module.exports = class CacheHandler {
     key = this.getKey("cache/" + key)
     console.log(`SET: ${key}`, ctx)
 
-    axios.put(`http://localhost:8083/kv/${key}`, payload)
+    const instance = axios.create()
+
+    instance.interceptors.request.use((config) => {
+        config.headers['request-startTime'] = process.hrtime()
+        return config
+    })
+
+    instance.interceptors.response.use((response) => {
+        const start = response.config.headers['request-startTime']
+        const end = process.hrtime(start)
+        const milliseconds = Math.round((end[0] * 1000) + (end[1] / 1000000))
+        response.headers['request-duration'] = milliseconds
+        return response
+    })
+
+    instance.put(`http://localhost:8083/kv/${key}`, payload)
+    .then(res => {
+      console.log(res.headers['request-duration'])
+    })
     .catch(err => {
       console.log('Error: ', err.message);
     });
