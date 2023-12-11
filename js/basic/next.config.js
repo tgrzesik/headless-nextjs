13b12c5aa1645aa7a8d8e7fd2e98ec082a63e3c6
ciaranshan1 <ciaran.shanahan@wpengine.com>
2023-12-11 12:58:16 +0000

    use normal cache handler

--- js/basic/next.config.js
+++ js/basic/next.config.js
@@ -73,7 +73,7 @@ const nextConfig = {
     ]
   },
   experimental: {
-    incrementalCacheHandlerPath: require.resolve('./.atlas/atlas-cache-handler.js'),
+    incrementalCacheHandlerPath: require.resolve('./atlas-cache-handler.js'),
     isrMemoryCacheSize: 0
   },
 }
