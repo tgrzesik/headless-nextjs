557a137bbeadf749e2d1db6e53c9c04b011e6036
Tomasz Grzesik <tomasz.grzesik@wpengine.com>
2024-09-26 15:13:09 +0200

    use edge-cache package

--- js/basic/pages/api/purge-paths.js
+++ js/basic/pages/api/purge-paths.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
     try {
         const paths = ['/sample-odisr']
         await purgePaths(paths)
+        return res.json({ purged: true })
     } catch (err) {
         console.log(err)
         return res.status(500).send('Error purging paths')
